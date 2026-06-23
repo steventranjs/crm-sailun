7c712c6422164cadcbd8719b1454e580ca05adac
Steven Tran <steven.tran@MacBook-Pro-9.local>
2026-06-24 00:13:01 +0400

    fix api

--- api/main.py
+++ api/main.py
@@ -2,15 +2,35 @@ from fastapi import FastAPI
 import requests
 import os
 
+from espo_api import EspoAPI
+
 app = FastAPI()
+
 CRM_URL = os.getenv("CRM_URL")
 
 @app.post("/submit-lead")
 def submit_lead(data: dict):
-    # API nhận từ web, đẩy thẳng vào EspoCRM
-    response = requests.post(f"{CRM_URL}/api/v1/Lead", json=data)
-    return response.json()
+    client = EspoAPI(CRM_URL, os.getenv("CRM_API_KEY"))
+    # data = {
+    #     'title': 'Mr',
+    #     'salutationName': 'John Tran',
+    #     'firstName': 'John',
+    #     'middleName': 'string',
+    #     'lastName': 'string',
+    #     'status': 'New',
+    #     'phoneNumber': '+11111-22222-33333',
+    #     'emailAddress': 'test@abc.com',
+    #     'description': 'Toi muon mua 1000 cai banh xe',
+    #     'source': 'xxx.com',
+    #     'opportunityAmount': 1000,
+    #     'opportunityAmountCurrency': 'USD',
+    #     'website': 'some-website-client',
+    #     'addressStreet': 'string',
+    #     'addressCity': "string",
+    #     'addressState': 'string',
+    #     'addressCountry': 'string',
+    #     'addressPostalCode': 'string',
+    # }
 
-@app.get("/get-leads/{supplier_id}")
-def get_leads(supplier_id: str):
-    return {"status": "success", "data": "Dữ liệu đã được ẩn contact theo logic của bạn"}
+    client.request('POST', 'Lead', data)
+    return response.json()
